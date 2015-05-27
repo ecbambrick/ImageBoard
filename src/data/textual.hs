e8d672c314f7ed6f87fe23e30893d3dafed391eb
Cole Bambrick <kcirbmab@gmail.com>
2015-05-27 18:32:58 -0400

    Added toLower function to Data.Textual class
    Included implementations for String, Data.Text.Text, and
    Data.Text.Lazy.Text

--- src/data/textual.hs
+++ src/data/textual.hs
@@ -3,19 +3,24 @@ module Data.Textual where
 
 import qualified Data.Text as Strict
 import qualified Data.Text.Lazy as Lazy
+import qualified Data.Char as Char
 
 class Textual a where
     splitOn :: a -> a -> [a]
-    strip :: a -> a
+    strip   :: a -> a
+    toLower :: a -> a
 
 instance Textual String where
     splitOn x = map Strict.unpack . Strict.splitOn (Strict.pack x) . Strict.pack
     strip = Strict.unpack . Strict.strip . Strict.pack
+    toLower = map Char.toLower
 
 instance Textual Strict.Text where
     splitOn = Strict.splitOn
     strip = Strict.strip
+    toLower = Strict.toLower
 
 instance Textual Lazy.Text where
     splitOn = Lazy.splitOn
     strip = Lazy.strip
+    toLower = Lazy.toLower
