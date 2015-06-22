32a24bf09ef82520823b30c4a8c7270c8a42f041
Cole Bambrick <kcirbmab@gmail.com>
2015-06-22 07:13:16 -0400

    Added Core.Tag.validate function
    Returns valid if all fields of the given tag are valid; otherwise
    invalid. Any validation that requires access to the database is ignored.

--- src/core/tag.hs
+++ src/core/tag.hs
@@ -1,8 +1,14 @@
 module Core.Tag where
 
-import Common            ( Tag(..), Entity(..), App, runDB )
+import Common            ( Tag(..), App, Entity, runDB )
+import Core.Validation   ( Property(..), Validation, isValidTag )
 import DataSource.SQLite ( selectTags )
 
 -- | Returns the list of all tag entities.
 get :: App [Entity Tag]
 get = runDB selectTags
+
+-- | Returns valid if all fields of the given tag are valid; otherwise invalid.
+-- | Any validation that requires access to the database is ignored.
+validate :: Tag -> Validation
+validate (Tag name) = isValidTag (Property "tag" name)
