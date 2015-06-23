5fc6571b4c37821d66d772475e28b848454d6115
Cole Bambrick <kcirbmab@gmail.com>
2015-06-22 20:35:03 -0400

    Refactored the Core.Validation module
    Added the following functions:
    * isFalse
    * isValid
    
    Removed the following functions as they were no longer needed:
    * allowNothing
    * atLeast
    * isNotEmpty
    
    Finally, modified isValidTag to consider empty or all-whitespace strings
    to be invalid.

--- src/core/validation.hs
+++ src/core/validation.hs
@@ -2,7 +2,7 @@ module Core.Validation where
 
 import Text.Printf  (printf)
 import Data.Char    (isAlphaNum, isSpace)
-import Data.Monoid  (Monoid, mempty, mappend)
+import Data.Monoid  (Monoid, (<>), mempty, mappend)
 
 ------------------------------------------------------------------------- Types
 
@@ -21,39 +21,31 @@ data Validation = Valid | Invalid [Error]
     deriving (Show)
 
 instance Monoid Validation where
-    mempty = Valid
-    mappend Valid b = b
-    mappend a Valid = a
+    mempty                          = Valid
+    mappend Valid b                 = b
+    mappend a Valid                 = a
     mappend (Invalid a) (Invalid b) = Invalid (a ++ b)
 
 -------------------------------------------------------------------- Validation
 
--- | Returns valid if a property is nothing; otherwise, applies the given
--- | validation function and returns the results.
-allowNothing :: (Property a -> Validation) -> Property (Maybe a) -> Validation
-allowNothing f (Property _ Nothing)     = Valid
-allowNothing f (Property name (Just x)) = f (Property name x)
-
--- | Returns valid if the given property is at least a given amount; otherwise,
--- | invalid.
-atLeast :: (Show a, Ord a, Num a) => a -> Property a -> Validation
-atLeast bound (Property name x)
-    | x < bound = Invalid [Error name (show x) "too small"]
-    | otherwise = Valid
+-- | Returns valid if the given property is false; otherwise, invalid.
+isFalse :: Property Bool -> Validation
+isFalse (Property name True) = Invalid [Error name "True" "expected false"]
+isFalse (Property _ False)   = Valid
 
 -- | Returns valid if the given property is a valid tag name; otherwise, 
 -- | invalid.
 isValidTag :: Property String -> Validation
 isValidTag (Property name x)
-    | all isValid x = Valid
-    | otherwise     = Invalid [Error name x "invalid characters"]
-    where
-        isValid x = isAlphaNum x || elem x "._- "
+    | null x            = Invalid [Error name x "empty"]
+    | all isSpace x     = Invalid [Error name x "only whitespace"]
+    | all isValidChar x = Valid
+    | otherwise         = Invalid [Error name x "invalid characters"]
+    where isValidChar x = isAlphaNum x || elem x "._- "
 
--- | Returns valid if the given property is not an empty string or a string 
--- | with only whitespace; otherwise, invalid.
-isNotEmpty :: Property String -> Validation
-isNotEmpty (Property name x)
-    | null x        = Invalid [Error name x "empty string"]
-    | all isSpace x = Invalid [Error name x "empty string"]
-    | otherwise     = Valid
+----------------------------------------------------------------------- Utility
+
+-- | Returns whether or not the given validation is Valid.
+isValid :: Validation -> Bool
+isValid (Invalid _) = False
+isValid Valid       = True
