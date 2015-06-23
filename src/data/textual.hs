38c391215884f4f10a5652f72848eeca25715d43
Cole Bambrick <kcirbmab@gmail.com>
2015-06-23 18:22:07 -0400

    Added replace function to the Data.Textual class

--- src/data/textual.hs
+++ src/data/textual.hs
@@ -6,21 +6,25 @@ import qualified Data.Text.Lazy as Lazy
 import qualified Data.Char as Char
 
 class Textual a where
+    replace :: a -> a -> a -> a
     splitOn :: a -> a -> [a]
     strip   :: a -> a
     toLower :: a -> a
 
 instance Textual String where
+    replace x y = Strict.unpack . Strict.replace (Strict.pack x) (Strict.pack y) . Strict.pack
     splitOn x = map Strict.unpack . Strict.splitOn (Strict.pack x) . Strict.pack
     strip = Strict.unpack . Strict.strip . Strict.pack
     toLower = map Char.toLower
 
 instance Textual Strict.Text where
+    replace = Strict.replace
     splitOn = Strict.splitOn
     strip = Strict.strip
     toLower = Strict.toLower
 
 instance Textual Lazy.Text where
+    replace = Lazy.replace
     splitOn = Lazy.splitOn
     strip = Lazy.strip
     toLower = Lazy.toLower
