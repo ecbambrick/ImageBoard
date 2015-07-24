9ad4a62e895d4c130118a713628f530356427f95
Cole Bambrick <kcirbmab@gmail.com>
2015-07-23 20:27:33 -0400

    Added Data.Textual.trim function
    Removes leading and trailing whitespace and reduces all consecutive
    spaces to single spaces.

--- src/data/textual.hs
+++ src/data/textual.hs
@@ -13,21 +13,25 @@ class Textual a where
     splitOn :: a -> a -> [a]
     strip   :: a -> a
     toLower :: a -> a
+    trim    :: a -> a
 
 instance Textual String where
     replace x y = unpack     . Strict.replace (pack x) (pack y) . pack
     splitOn x   = map unpack . Strict.splitOn (pack x)          . pack
     strip       = unpack     . Strict.strip                     . pack
     toLower     = map Char.toLower
+    trim        = unwords . words
 
 instance Textual Strict.Text where
     replace     = Strict.replace
     splitOn     = Strict.splitOn
     strip       = Strict.strip
     toLower     = Strict.toLower
+    trim        = Strict.unwords . Strict.words
 
 instance Textual Lazy.Text where
     replace     = Lazy.replace
     splitOn     = Lazy.splitOn
     strip       = Lazy.strip
     toLower     = Lazy.toLower
+    trim        = Lazy.unwords . Lazy.words
