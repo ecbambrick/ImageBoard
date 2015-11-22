feae8973a8a9b7a1ae6d3c652a4af3137b64e43e
Cole Bambrick <kcirbmab@gmail.com>
2015-11-22 12:05:17 -0500

    Refactored App.View module

--- src/app/view.hs
+++ src/app/view.hs
@@ -34,39 +34,43 @@ instance PrintfArg IndexType where
 -- | Renders a page for the given album as text containing HTML.
 albumPage :: Entity Album -> Text
 albumPage (Entity id album @ Album {..}) =
-    render $ document (printf "Album %i" id) [] $ do
+    render title [] $ do
+        tags_    albumTagNames
+        gallery_ albumPages
         
-        tags albumTagNames
-    
-        forM_ albumPages $ \(Page _ number _) ->
+    where title    = "Album " ++ show id
+          gallery_ = mapM_ $ \(Page _ number _) ->
             let page  = Album.getPage album number
                 url   = pack $ printf "/album/%i/%i" id number
                 thumb = pack $ maybe "" (Path.getPageThumbnailURL id) page
-            
-            in a_ [href_ url] $ img_ [src_ thumb]
+
+            in a_ [href_ url] $ img_ [src_ thumb] :: Html ()
 
 -- | Renders a page for the given album as text containing HTML.
 albumsPage :: String -> Int -> Int -> Int -> [Entity Album] -> Text
 albumsPage query page total pageSize albums = 
-    render $ document (printf "Albums (%i)" total) [] $
-        div_ [class_ "content"] $ do
-        
-            div_ [class_ "header"] $ do
-                div_ [class_ "left" ] $ searchForm Albums query
-                div_ [class_ "right"] $ uploadForm
-                
-            div_ [class_ "gallery"] $ do
-                prevPage Albums page query
-                nextPage Albums page query total pageSize
-                forM_ albums albumThumbnail
+    render title [] $ do
+        div_ [class_ "header"] $ do
+            div_ [class_ "left" ] $ searchForm_ Albums query
+            div_ [class_ "right"] $ uploadForm_            
+        div_ [class_ "gallery"] $ do
+            prevPage_ Albums page query
+            nextPage_ Albums page query total pageSize
+            gallery_  albums
+    
+    where title    = printf "Albums (%i)" total
+          gallery_ = mapM_ $ \album @ (Entity id _) ->
+            let url   = pack $ printf "/album/%i" id
+                thumb = pack $ Path.getAlbumThumbnailURL album
+            
+            in a_ [href_ url] $ img_ [src_ thumb] :: Html ()
 
 -- | Renders a page for the given image as text containing HTML.
 imagePage :: String -> Entity Image -> Entity Image -> Entity Image -> Text
 imagePage query (Entity prev _) (Entity id image @ Image {..}) (Entity next _) =
-    render $ document title scripts $ do
-    
-        tags imageTagNames
-        img_ [id_ "image", class_ "display", src_ url]
+    render title scripts $ do
+        tags_ imageTagNames
+        img_  [id_ "image", class_ "display", src_ url]
     
     where title   = "Image " ++ show id
           url     = pack (Path.getImageURL image)
@@ -75,40 +79,33 @@ imagePage query (Entity prev _) (Entity id image @ Image {..}) (Entity next _) =
                     , imageScript prev next query ]
 
 -- | Renders a page for the given image as text containing HTML.
-imagesPage :: String -> Int -> Int ->  Int -> [Entity Image] -> Text
+imagesPage :: String -> Int -> Int -> Int -> [Entity Image] -> Text
 imagesPage query page total pageSize images =
-    render $ document (printf "Images (%i)" total) [] $
-        div_ [class_ "content"] $ do
-        
-            div_ [class_ "header"] $ do
-                div_ [class_ "left" ] $ searchForm Images query
-                div_ [class_ "right"] $ uploadForm
+    render title [] $ do
+        div_ [class_ "header"] $ do
+            div_ [class_ "left" ] $ searchForm_ Images query
+            div_ [class_ "right"] $ uploadForm_
+        div_ [class_ "gallery"] $ do
+            prevPage_ Images page query
+            nextPage_ Images page query total pageSize
+            gallery_  images
                 
-            div_ [class_ "gallery"] $ do
-                prevPage Images page query
-                nextPage Images page query total pageSize
-                forM_ images imageThumbnail
-        
-    where imageThumbnail :: Entity Image -> Html ()
-          imageThumbnail (Entity id image) = a_ [href_ url] $ img_ [src_ thumb]
-            where url   = pack $ printf "/image/%i%s" id q
-                  thumb = pack $ Path.getImageThumbnailURL image
-          q = if null query then "" else "?q=" ++ query
+    where title    = printf "Images (%i)" total
+          query'   = if null query then "" else "?q=" ++ query
+          gallery_ = mapM_ $ \(Entity id image) ->
+            let url   = pack $ printf "/image/%i%s" id query'
+                thumb = pack $ Path.getImageThumbnailURL image
+
+            in a_ [href_ url] $ img_ [src_ thumb] :: Html ()
 
 -- | Renders a page for the given album page as text containing HTML.
 pagePage :: ID -> Page -> Text
-pagePage id page @ Page {..} =
-    render $ document (printf "Album %i - page %i" id pageNumber) [] $ 
-        img_ [src_ (pack (Path.getPageURL id page))]
+pagePage id page @ Page {..} = render title [] (img_ [src_ url])
+    
+    where title = printf "Album %i - page %i" id pageNumber
+          url   = pack (Path.getPageURL id page)
 
 -------------------------------------------------------------------- Components
-
--- | Returns a link to an album that is displayed as a thumbnail.
-albumThumbnail :: Entity Album -> Html ()
-albumThumbnail album @ (Entity id _) = a_ [href_ url] $ img_ [src_ thumb]
-    
-    where url   = pack $ printf "/album/%i" id
-          thumb = pack $ Path.getAlbumThumbnailURL album
 
 -- | Creates an HTML document with the given title, list of javascript import
 -- | paths and HTML child as the body.
@@ -122,37 +119,37 @@ document title imports f = doctypehtml_ $ do
     body_ f
 
 -- | Returns a link to the next page of post results.
-nextPage :: IndexType -> Int -> String -> Int -> Int -> Html ()
-nextPage post page query total size = 
-    if page * size >= total 
+nextPage_ :: IndexType -> Int -> String -> Int -> Int -> Html ()
+nextPage_ post page query total pageSize =  
+    if page * pageSize >= total 
         then mempty 
         else link
-
-    where link = a_ [href_ url] $ div_ [class_ "thumb"] "next" 
-          url  = pack $ printf "/%s?page=%i%s" post (page + 1) q
-          q    = if null query then "" else "&q=" ++ query
+    
+    where link   = a_ [href_ url] $ div_ [class_ "thumb"] "next" 
+          url    = pack $ printf "/%s?page=%i%s" post (page + 1) query'
+          query' = if null query then "" else "&q=" ++ query
 
 -- | Returns a link to the previous page of post results.
-prevPage :: IndexType -> Int -> String -> Html ()
-prevPage post page query = 
+prevPage_ :: IndexType -> Int -> String -> Html ()
+prevPage_ post page query = 
     if page <= 1 
         then mempty 
         else link
     
-    where link = a_ [href_ url] $ div_ [class_ "thumb"] "previous"
-          url  = pack $ printf "/%s?page=%i%s" post (page - 1) q
-          q    = if null query then "" else "&q=" ++ query
+    where link   = a_ [href_ url] $ div_ [class_ "thumb"] "previous"
+          url    = pack $ printf "/%s/?page=%i%s" post (page - 1) query'
+          query' = if null query then "" else "&q=" ++ query
 
 -- | Returns an element containing tags based on the given list of tag names.
-tags :: [String] -> Html ()
-tags tagNames = 
+tags_ :: [String] -> Html ()
+tags_ tagNames = 
     unless (null tagNames) $
         ul_ [id_ "tags", class_ "listBox"] $ 
             forM_ tagNames (li_ . toHtml)
 
 -- | Returns a search form for filtering posts.
-searchForm :: IndexType -> String -> Html ()
-searchForm post query = form_ 
+searchForm_ :: IndexType -> String -> Html ()
+searchForm_ post query = form_ 
     [ name_ "search"
     , action_ (pack $ show post)
     , method_ "get" ] $ do
@@ -162,8 +159,8 @@ searchForm post query = form_
     where query' = if null query then "" else pack query
 
 -- | Returns an upload form for uploading a new post.
-uploadForm :: Html ()
-uploadForm = form_ 
+uploadForm_ :: Html ()
+uploadForm_ = form_ 
     [ name_ "upload"
     , action_ "upload"
     , method_ "post"
@@ -193,6 +190,7 @@ imageScript prev next query =
 
 ----------------------------------------------------------------------- Utility
 
--- | Renders the given HTML as text.
-render :: Html a -> Text
-render = toStrict . renderText
+-- | Renders the given HTML body as text, using the given title and imports to
+-- | generate a head element.
+render :: String -> [Html ()] -> Html a -> Text
+render title imports f = toStrict $ renderText $ document title imports f
