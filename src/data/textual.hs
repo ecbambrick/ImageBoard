1f8fcb31e32723f875adabea0a96f4976a563cde
Cole Bambrick <kcirbmab@gmail.com>
2015-12-29 17:28:48 -0500

    Added Textual.display function
    Converts the value to readable text.

--- src/data/textual.hs
+++ src/data/textual.hs
@@ -10,6 +10,7 @@ import qualified Data.Text.Lazy as Lazy
 import Data.Text ( unpack, pack )
 
 class Textual a where
+    display     :: (Show b) => b -> a
     intercalate :: a -> [a] -> a
     replace     :: a -> a -> a -> a
     splitOn     :: a -> a -> [a]
@@ -18,25 +19,28 @@ class Textual a where
     trim        :: a -> a
 
 instance Textual String where
+    display     = show
+    intercalate = List.intercalate
     replace x y = unpack     . Strict.replace (pack x) (pack y) . pack
     splitOn x   = map unpack . Strict.splitOn (pack x)          . pack
     strip       = unpack     . Strict.strip                     . pack
     toLower     = map Char.toLower
     trim        = unwords . words
-    intercalate = List.intercalate
 
 instance Textual Strict.Text where
+    display     = Strict.pack . show
+    intercalate = Strict.intercalate
     replace     = Strict.replace
     splitOn     = Strict.splitOn
     strip       = Strict.strip
     toLower     = Strict.toLower
     trim        = Strict.unwords . Strict.words
-    intercalate = Strict.intercalate
 
 instance Textual Lazy.Text where
+    display     = Lazy.pack . show
+    intercalate = Lazy.intercalate
     replace     = Lazy.replace
     splitOn     = Lazy.splitOn
     strip       = Lazy.strip
     toLower     = Lazy.toLower
     trim        = Lazy.unwords . Lazy.words
-    intercalate = Lazy.intercalate
