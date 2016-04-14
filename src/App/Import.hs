09b58cda84f010343033c8d6fbe48926a8a4e304
ecbambrick <kcirbmab@gmail.com>
2016-04-13 20:23:46 -0400

    Modified App.Import.fromDirectory to log errors

--- src/App/Import.hs
+++ src/App/Import.hs
@@ -8,15 +8,14 @@ import qualified Text.Parsec    as Parsec
 
 import App.Config             ( loadConfig )
 import App.FileType           ( FileType(..), getFileType )
-import Control.Applicative    ( (<|>) )
-import Control.Monad.Reader   ( void, forM_, runReaderT )
-import Data.Functor.Identity  ( Identity )
+import App.Validation         ( Validation(..), isValid )
+import Control.Monad.Reader   ( MonadIO, liftIO, unless, void, forM_, runReaderT )
 import Data.Textual           ( splitOn, intercalate )
 import System.FilePath        ( (</>), dropExtension )
 import System.Directory       ( getDirectoryContents )
-import Text.Parsec            ( ParsecT, ParseError, Stream, anyChar, anyToken
-                              , between, char, choice, eof, lookAhead, many
-                              , manyTill, noneOf, option, spaces, try )
+import Text.Parsec            ( ParsecT, ParseError, Stream, (<|>), anyChar
+                              , anyToken, between, char, choice, eof, lookAhead
+                              , many, manyTill, noneOf, option, spaces, try )
 
 -- | Import each valid file in the given directory into the database.
 fromDirectory :: FilePath -> IO ()
@@ -31,11 +30,24 @@ fromDirectory path = do
                 filePath     = path </> fileName
 
             in case parseResults of
-                Left err            -> return ()
-                Right (title, tags) -> case fileResults of
-                    ArchiveType file -> void $ Album.insert file title tags
-                    ImageType   file -> void $ Image.insert file title tags
-                    InvalidType    _ -> return ()
+                Left err -> do
+                    logError filePath "invalid filename"
+
+                Right (title, tags) -> do
+                    result <- case fileResults of
+                        ArchiveType file -> Album.insert file title tags
+                        ImageType   file -> Image.insert file title tags
+                        InvalidType    _ -> return Valid
+
+                    unless (isValid result) $ do
+                        logError filePath (show result)
+
+----------------------------------------------------------------------- Utility
+
+-- | Logs an import error message to the console.
+logError :: (MonadIO m) => FilePath -> String -> m ()
+logError path message =
+    liftIO $ putStrLn $ "Failed to import \"" ++ path ++ "\": " ++ message
 
 -- | Parse the given string and return the extracted title and list of tags.
 parse :: String -> Either ParseError (String, [String])
