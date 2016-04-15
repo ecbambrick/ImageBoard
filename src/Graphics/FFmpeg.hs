a83c7d71a48fc86cb19ca6f03a4a684b7bbc6521
ecbambrick <kcirbmab@gmail.com>
2016-04-15 18:39:29 -0400

    Modified Graphics.FFmpeg to not use stdout/stderr

--- src/Graphics/FFmpeg.hs
+++ src/Graphics/FFmpeg.hs
@@ -1,30 +1,49 @@
 module Graphics.FFmpeg where
 
-import System.Directory  ( createDirectoryIfMissing )
-import System.FilePath   ( takeDirectory )
-import System.Process    ( callProcess, readProcess )
-import Text.Regex        ( matchRegex, mkRegex )
+import qualified Data.ByteString as ByteString
 
--- | Generates a thumbnail of the given size for the image or video file at
--- | the first given file path and save it to the second given file path. If
--- | the path does not exist, it will be created. Any existing file will be
--- | overwritten.
+import Control.DeepSeq  ( deepseq )
+import System.Directory ( createDirectoryIfMissing )
+import System.FilePath  ( takeDirectory )
+import System.Process   ( CreateProcess(..), StdStream(..), createProcess, proc )
+import System.IO        ( hClose, hGetContents )
+import Text.Regex       ( matchRegex, mkRegex )
+
+-- | Generates a thumbnail with a maximum dimension of the given size for the
+-- | image or video file at the first given file path and save it to the second
+-- | given file path. If the second path does not exist, it will be created.
+-- | Any existing file will be overwritten.
 createThumbnail :: Int -> FilePath -> FilePath -> IO ()
 createThumbnail size from to = do
+    let process = (proc name args) { std_out = CreatePipe, std_err = CreatePipe }
+        name    = "ffmpeg"
+        args    = [ "-i", from
+                  , "-y"
+                  , "-vframes", "1"
+                  , "-filter:v", "scale=min(" ++ show size ++ "\\, iw):-1"
+                  , to ]
+
     createDirectoryIfMissing True (takeDirectory to)
-    callProcess "ffmpeg"
-        [ "-i", from
-        , "-y"
-        , "-vframes", "1"
-        , "-filter:v", "scale=min(" ++ show size ++ "\\, iw):-1"
-        , to ]
+
+    -- Evaluate output to prevent bug where thumbnails don't get generated.
+    (_, _, Just stderr, _) <- createProcess process
+    ByteString.hGetContents stderr
+    hClose stderr
 
 -- | Returns the width and height of the given image or video file.
 getDimensions :: FilePath -> IO (Int, Int)
 getDimensions path = do
-    results <- readProcess "ffprobe" ["-show_entries", "stream=height,width", path] []
+    let process = (proc name args) { std_out = CreatePipe, std_err = CreatePipe }
+        name    = "ffprobe"
+        args    = [ "-show_entries"
+                  , "stream=width,height"
+                  , path ]
 
-    let Just [w] = matchRegex (mkRegex "^width=([0-9]+)$")  results
-        Just [h] = matchRegex (mkRegex "^height=([0-9]+)$") results
+    (_, Just stdout, _, _) <- createProcess process
+    results                <- hGetContents stdout
+    results `deepseq` hClose stdout
 
-    return (read w, read h)
+    let Just [width]  = mkRegex "^width=([0-9]+)$"  `matchRegex` results
+        Just [height] = mkRegex "^height=([0-9]+)$" `matchRegex` results
+
+    return (read width, read height)
