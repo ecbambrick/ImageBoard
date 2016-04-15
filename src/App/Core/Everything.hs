6a4796cc55f9059c4e8aca9b26a5485092e3928a
ecbambrick <kcirbmab@gmail.com>
2016-04-14 21:54:45 -0400

    Added App.Core.Everything module
    Used for managing all application data as a whole.

---
+++ src/App/Core/Everything.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE RankNTypes       #-}
+
+module App.Core.Everything where
+
+import qualified App.Database as Database
+import qualified App.Path     as Path
+
+import App.Common          ( App )
+import Control.Monad       ( when )
+import Control.Monad.Trans ( liftIO )
+import System.Directory    ( doesDirectoryExist, removeDirectoryRecursive )
+
+-- | Deletes all data from the database.
+delete :: App ()
+delete = do
+    dataPath   <- Path.getDataPath
+    pathExists <- liftIO $ doesDirectoryExist dataPath
+
+    liftIO $ when pathExists $ removeDirectoryRecursive dataPath
+
+    Database.deleteDatabase
+    Database.createDatabase
+
+-- | Initializes a new database if one does not already exist.
+initialize :: App ()
+initialize = Database.createDatabase
