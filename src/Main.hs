87d5c903af76843732e374a663eb7d6abfee9338
ecbambrick <kcirbmab@gmail.com>
2016-04-22 21:00:17 -0400

    Added delete-all-data command
    Deletes all data from the database.

--- src/Main.hs
+++ src/Main.hs
@@ -4,10 +4,15 @@
 module Main where
 
 import qualified App.Control         as Application
+import qualified App.Core.Everything as Everything
 import qualified App.Import          as Import
 import qualified App.Path            as Path
 import qualified App.Server          as Server
 import qualified System.Console.Args as CLI
+
+import Control.Monad.Reader ( liftIO, when )
+import Data.Functor         ( (<$>) )
+import Data.Textual         ( toLower )
 
 main = CLI.cli "Image board." $ do
 
@@ -24,3 +29,20 @@ main = CLI.cli "Image board." $ do
 
         runApplication $ do
             Import.fromDirectory path
+
+    -- Delete all data from the database.
+    CLI.command "delete-all-data" $ do
+        autoYes <- CLI.option ('y', "auto-yes") "Automatically answer yes to prompts."
+
+        runApplication $ do
+            continue <- if autoYes
+                then do
+                    return True
+                else do
+                    liftIO $ putStrLn "Are you sure you want to delete all data?"
+                    response <- liftIO $ toLower <$> getLine
+
+                    return (response == "y" || response == "yes")
+
+            when continue $ do
+                Everything.delete
