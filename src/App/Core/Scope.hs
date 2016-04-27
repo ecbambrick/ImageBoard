6eae1bcbfef2a6afe9a6374c5500a691f8463588
ecbambrick <kcirbmab@gmail.com>
2016-04-27 17:21:51 -0400

    Added App.Core.Scope module
    Contains functions for dealing with scopes.

---
+++ src/App/Core/Scope.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE RankNTypes       #-}
+
+module App.Core.Scope ( delete, insertOrUpdate, querySingle ) where
+
+import qualified App.Database as DB
+
+import App.Control           ( runDB )
+import App.Core.Types        ( Scope(..), App )
+import App.Validation        ( Error(..), Validation, isValid, verify )
+import Control.Monad         ( void, when )
+import Data.Functor.Extended ( (<$$>) )
+import Database.Engine       ( Entity(..), fromEntity )
+
+-------------------------------------------------------------------------- CRUD
+
+-- | Deletes the scope with the given name.
+delete :: String -> App ()
+delete name = runDB $ DB.deleteScope name
+
+-- | Inserts a new scope into the database using the given name and expression
+-- | or updates an existing scope if one with the same name already exists.
+insertOrUpdate :: String -> String -> App Validation
+insertOrUpdate name expression = runDB $ do
+    let scope   = Scope name expression
+        results = validate scope
+
+    when (isValid results) $ do
+        existingScope <- DB.selectScope name
+
+        case existingScope of
+            Nothing            -> void $ DB.insertScope scope
+            Just (Entity id _) -> void $ DB.updateScope (Entity id scope)
+
+    return results
+
+-- | Returns the scope with the given name.
+querySingle :: String -> App (Maybe Scope)
+querySingle "all" = return (Just (Scope "all" ""))
+querySingle name  = fromEntity <$$> runDB (DB.selectScope name)
+
+----------------------------------------------------------------------- Utility
+
+-- | Returns valid if the given scope is valid; otherwise invalid.
+validate :: Scope -> Validation
+validate (Scope name _) =
+    let nameError = Error "name" name "name cannot be 'all'"
+    in verify (name /= "all") nameError
