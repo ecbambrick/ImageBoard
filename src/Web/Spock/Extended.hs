71b7bb7157c0858e40b0f53a2527ee120ae63c69
ecbambrick <kcirbmab@gmail.com>
2016-04-29 18:54:20 -0400

    Added status response functions to the Web.Spock.Extended module
    Allows returning 404 and 500 responses to requests.

--- src/Web/Spock/Extended.hs
+++ src/Web/Spock/Extended.hs
@@ -1,24 +1,42 @@
 module Web.Spock.Extended where
 
+import Web.Spock as Spock
+
 import Data.Functor        ( Functor, (<$>) )
 import Data.HashMap.Strict ( (!) )
 import Data.Maybe          ( fromMaybe )
-import Data.Text           ( Text, unpack )
+import Data.Text           ( Text, pack, unpack )
 import Control.Monad.Trans ( MonadIO )
-import Web.Spock           ( ActionT, UploadedFile(..), files, param )
+import Network.HTTP.Types  ( status404, status500 )
 import Web.PathPieces      ( PathPiece )
 
--- | Get the uploaded file with the given input field name.
-getFile :: (Functor m, MonadIO m) => Text -> ActionT m (String, String, String)
-getFile key = do
-    uploadedFile <- (! key) <$> files
+-- Return a 404 status.
+notFound :: (MonadIO m) => ActionCtxT ctx m a
+notFound = do
+    Spock.setStatus status404
+    Spock.html $ pack $ concat
+        [ "<html>"
+        , "<head><title>404 - File not found</title></head>"
+        , "<body><h1>404 - File not found</h1></body>"
+        , "</html>" ]
 
-    let name        = unpack (uf_name uploadedFile)
-        contentType = unpack (uf_contentType uploadedFile)
-        location    = uf_tempLocation uploadedFile
+-- Return a 500 status.
+serverError :: (MonadIO m) => Text -> ActionCtxT ctx m a
+serverError text = do
+    Spock.setStatus status500
+    Spock.text text
+
+-- | Get the uploaded file with the given input field name.
+getFile :: (Functor m, MonadIO m) => Text -> Spock.ActionT m (String, String, String)
+getFile key = do
+    uploadedFile <- (! key) <$> Spock.files
+
+    let name        = unpack (Spock.uf_name uploadedFile)
+        contentType = unpack (Spock.uf_contentType uploadedFile)
+        location    = Spock.uf_tempLocation uploadedFile
 
     return (name, contentType, location)
 
 -- | Returns a request param or a default value if one it could not be found.
 optionalParam :: (PathPiece p, Functor m, MonadIO m) => Text -> p -> ActionT m p
-optionalParam name defaultValue = fromMaybe defaultValue <$> param name
+optionalParam name defaultValue = fromMaybe defaultValue <$> Spock.param name
