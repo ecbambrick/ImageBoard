86a61c614493ca92cc966eb109a549bf9b864bfd
ecbambrick <kcirbmab@gmail.com>
2016-09-04 10:16:45 -0400

    Fixed bug where ffmpeg/ffprobe processes were being left open

--- src/Graphics/FFmpeg.hs
+++ src/Graphics/FFmpeg.hs
@@ -1,12 +1,13 @@
 module Graphics.FFmpeg where
 
-import qualified Data.ByteString as ByteString
+import qualified Data.ByteString  as ByteString
+import qualified System.Directory as Dir
+import qualified System.FilePath  as FilePath
+import qualified System.IO        as IO
+import qualified System.Process   as Process
 
-import Control.DeepSeq  ( deepseq )
-import System.Directory ( createDirectoryIfMissing )
-import System.FilePath  ( takeDirectory )
-import System.Process   ( CreateProcess(..), StdStream(..), createProcess, proc )
-import System.IO        ( hClose, hGetContents )
+import Control.Monad    ( void )
+import System.Process   ( CreateProcess(..), StdStream(..) )
 import Text.Regex       ( matchRegex, mkRegex )
 
 -- | Generates a thumbnail with a maximum dimension of the given size for the
@@ -15,7 +16,7 @@ import Text.Regex       ( matchRegex, mkRegex )
 -- | Any existing file will be overwritten.
 createThumbnail :: Int -> FilePath -> FilePath -> IO ()
 createThumbnail size from to = do
-    let process = (proc name args) { std_out = CreatePipe, std_err = CreatePipe }
+    let process = (Process.proc name args) { std_out = CreatePipe, std_err = CreatePipe }
         name    = "ffmpeg"
         args    = [ "-i", from
                   , "-y"
@@ -23,25 +24,22 @@ createThumbnail size from to = do
                   , "-filter:v", "scale=min(" ++ show size ++ "\\, iw):-1"
                   , to ]
 
-    createDirectoryIfMissing True (takeDirectory to)
-
-    -- Evaluate output to prevent bug where thumbnails don't get generated.
-    (_, _, Just stderr, _) <- createProcess process
-    ByteString.hGetContents stderr
-    hClose stderr
+    Dir.createDirectoryIfMissing True (FilePath.takeDirectory to)
+    (_, _, _, handle) <- Process.createProcess process
+    void $ Process.waitForProcess handle
 
 -- | Returns the width and height of the given image or video file.
 getDimensions :: FilePath -> IO (Int, Int)
 getDimensions path = do
-    let process = (proc name args) { std_out = CreatePipe, std_err = CreatePipe }
+    let process = (Process.proc name args) { std_out = CreatePipe, std_err = CreatePipe }
         name    = "ffprobe"
         args    = [ "-show_entries"
                   , "stream=width,height"
                   , path ]
 
-    (_, Just stdout, _, _) <- createProcess process
-    results                <- hGetContents stdout
-    results `deepseq` hClose stdout
+    (_, Just stdout, _, handle) <- Process.createProcess process
+    results                     <- IO.hGetContents stdout
+    Process.waitForProcess handle
 
     let Just [width]  = mkRegex "^width=([0-9]+)$"  `matchRegex` results
         Just [height] = mkRegex "^height=([0-9]+)$" `matchRegex` results
